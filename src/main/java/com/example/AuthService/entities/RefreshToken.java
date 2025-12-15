338b2c1afa567fee301e80eae0299dbbbcf002e8
Harshith Muthangi <harshithm2508@gmail.com>
2025-12-15 17:09:53 +0530

    entities done

--- src/main/java/com/example/AuthService/entities/RefreshToken.java
+++ src/main/java/com/example/AuthService/entities/RefreshToken.java
@@ -1,4 +1,35 @@
 package com.example.AuthService.entities;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+
+import java.time.Instant;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+@Table(name = "tokens")
 public class RefreshToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String token;
+
+    private Instant expiryDate;
+
+    @OneToOne
+    @JoinColumn(name = "id", referencedColumnName = "user_id")
+    private UserInfo userInfo;
 }
--- src/main/java/com/example/AuthService/entities/UserInfo.java
+++ src/main/java/com/example/AuthService/entities/UserInfo.java
@@ -1,4 +1,35 @@
 package com.example.AuthService.entities;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class UserInfo {
+
+    @Id
+    @Column(name = "User_id")
+    private String userId;
+
+    private String username;
+
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<UserRole> roles = new HashSet<>();
 }
