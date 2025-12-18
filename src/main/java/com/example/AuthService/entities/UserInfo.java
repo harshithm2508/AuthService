3765e133579649174ffd8b92f31e24fc30a935d6
Harshith Muthangi <harshithm2508@gmail.com>
2025-12-18 11:22:27 +0530

    CustomUserDetails service

--- src/main/java/com/example/AuthService/entities/UserInfo.java
+++ src/main/java/com/example/AuthService/entities/UserInfo.java
@@ -17,13 +17,41 @@ import java.util.Set;
 @ToString
 public class UserInfo {
 
+    public String getUserId() {
+        return userId;
+    }
+
     @Id
     @Column(name = "User_id")
     private String userId;
 
     private String username;
 
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     private String password;
+
+    public Set<UserRole> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<UserRole> roles) {
+        this.roles = roles;
+    }
 
     @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(
