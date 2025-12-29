c76445a4d940355d2e16ba87dce3b855fe986851
Harshith Muthangi <harshithm2508@gmail.com>
2025-12-29 09:55:51 +0530

    request and response repo

---
+++ src/main/java/com/example/AuthService/request/RefreshTokenDTO.java
@@ -0,0 +1,14 @@
+package com.example.AuthService.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class RefreshTokenDTO {
+    private String token;
+}
